7015b8cd3bae28e7a193c500b3185a2c31d6fb1a
dev-thakku <jemshithtk@gmail.com>
2020-09-12 21:13:44 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { response } = require("express");
+const stripe = require("stripe")(
+  "sk_test_51HQCEqEkyZimy8Tzmm0K7OiIn4d9FbAJZxI51R4fZjuCGIuNtiz1ULdc1ECJ33obiYAMvwx2T8ckMqzEeq8uWhc500Df5OFExA"
+);
+const port = 3001;
+
+// - API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "inr",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen Command
+
+app.listen(port, () => {
+  console.log(`Amazon-Backend listening at http://localhost:${port}`);
+});
